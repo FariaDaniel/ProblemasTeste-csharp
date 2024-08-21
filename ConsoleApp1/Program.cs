d9dba1dc74b5856092e9ad5caa9c415703920695
Daniel Faria <danielfaria097@outlook.com>
2024-08-21 15:26:18 -0300

    Programa teste Enum

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp1.Entities;
+using ConsoleApp1.Entities.Enums;
+using System;
+using System.Collections.Generic;
+
+
+namespace Couse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
+
+
+
+/*
+    OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+Se mesmo assim ainda tiver dando erro, há ainda uma terceira forma:
+
+OrderStatus os;
+Enum.TryParse("Delivered", out os);
+*/
